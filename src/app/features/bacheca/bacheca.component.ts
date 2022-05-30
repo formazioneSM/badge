c75713149b091cdbf9d42d938bf545ce3f3f57be
Luigi <lrusso@sysmanagement.it>
2022-05-30 12:25:29 +0200

    Aggiunto servizio bacheca

--- src/app/features/bacheca/bacheca.component.ts
+++ src/app/features/bacheca/bacheca.component.ts
@@ -1,37 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import {Subscription, timer } from 'rxjs';
-
+import { Subscription, timer } from 'rxjs';
+import { BachecaService } from '../../shared/uikit/services/bacheca.service';
 
 @Component({
   selector: 'app-bacheca',
   templateUrl: './bacheca.component.html',
-  styleUrls: ['./bacheca.component.css']
+  styleUrls: ['./bacheca.component.css'],
 })
 export class BachecaComponent implements OnInit {
-
-  toast:boolean = false;
+  toast: boolean = false;
   startTimer: Subscription | undefined;
-  card:boolean = true;
-  constructor() { }
+  card: boolean = true;
+  posts: any = [];
+  constructor(private bachecaService: BachecaService) {}
 
   ngOnInit(): void {
+    this.loadPosts();
   }
 
- showToast(){
-  this.card = false;
-   this.toast = true;
-   this.startTimer = timer(3000).subscribe(() => {
-     console.log('ciao')
-     this.toast = false;
-    })
+  showToast() {
+    this.card = false;
+    this.toast = true;
+    this.startTimer = timer(3000).subscribe(() => {
+      console.log('ciao');
+      this.toast = false;
+    });
+  }
 
- }
+  stopDelete(e: any) {
+    this.startTimer?.unsubscribe();
+    this.card = true;
+    this.toast = false;
+  }
 
- stopDelete(e:any){
-   
-   this.startTimer?.unsubscribe();
-   this.card = true;
-   this.toast = false;
-
- }
+  loadPosts() {
+    this.bachecaService.getAllPosts().subscribe((posts) => {
+        this.posts = posts;
+        console.log(this.posts);
+      });
+  }
 }
