c7d9eb98c6591f067d216b41541af12364d33528
Ilaria <idellaquila@sysmanagement.it>
2022-05-30 12:20:05 +0200

    modifica auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,22 +6,21 @@ import { NgxPermissionsGuard, NgxPermissionsModule } from 'ngx-permissions';
 
 
 const routes: Routes = [
-  {path:'home', loadChildren: ()=> import('./features/home/home.module').then(m => m.HomeModule),canActivate: [NgxPermissionsGuard],data: {
+  {path:'home', loadChildren: ()=> import('./features/home/home.module').then(m => m.HomeModule),canLoad:[NgxPermissionsGuard],data: {
     permissions: {
       only: ['ADMIN', 'USER'],
-      except: ['GUEST']
+      redirectTo:'login'
     }
   }},
   {path:'login', loadChildren: ()=> import('./features/login/login.module').then(m => m.LoginModule)},
   {path: 'add_Content', loadChildren: () => import('./features/aggiungi/aggiungi.module').then(m => m.AddContentModule) },
   {path:'register',loadChildren: () => import('./features/register/register.module').then(m => m.RegisterModule)},
   {path:'passwordrecovery',loadChildren: () => import('./features/passwordrecovery/password-recovery.module').then(m =>m.PasswordRecoveryModule)},
-
+  {path:'**', redirectTo:'home'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes),
-    NgxPermissionsModule.forChild()
+  imports: [RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
 })
