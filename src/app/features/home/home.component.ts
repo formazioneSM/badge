6131ef9f931b3ee9da66f805a22ece705806e47f
Luigi <lrusso@sysmanagement.it>
2022-06-08 13:00:09 +0200

    aggiunta animazione slide in al cambio rotta

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,63 +1,70 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ScrollService } from 'src/app/shared/uikit/services/scroll/scroll.service';
 import { NgxPermissionsService } from 'ngx-permissions';
-import {  Router } from '@angular/router';
+import { ChildrenOutletContexts, Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/uikit/services/auth/auth.service';
 import { ToastService } from 'src/app/shared/uikit/services/toast.service';
+import { slideInAnimation } from 'src/app/shared/utils/animation';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [slideInAnimation],
 })
 export class HomeComponent implements OnInit {
-
   actualScroll: number | undefined;
-  scroll:boolean = false;
+  scroll: boolean = false;
   // admin:boolean = true;
   user = true;
 
-
-  constructor(private _scrollService: ScrollService,public permissions:NgxPermissionsService, private router:Router, 
-    private authService : AuthService,public toastService:ToastService) {
-    this._scrollService.getScroll().subscribe(s => {
+  constructor(
+    private _scrollService: ScrollService,
+    public permissions: NgxPermissionsService,
+    private router: Router,
+    private authService: AuthService,
+    public toastService: ToastService,
+    private contexts: ChildrenOutletContexts
+  ) {
+    this._scrollService.getScroll().subscribe((s) => {
       this.actualScroll = s;
-    })
+    });
   }
-
 
   ngOnInit(): void {
     // let token = this.authService.apiToken
     // this.isAdminOrUser(token)
-   console.log(this.permissions.getPermissions())
-
-
+    console.log(this.permissions.getPermissions());
   }
 
   changeScroll(e: any) {
-    this._scrollService.setScroll(e.target.scrollTop)
+    this._scrollService.setScroll(e.target.scrollTop);
     this.scroll = true;
   }
 
   @ViewChild('scrollToTop') scrollToTop: ElementRef | any;
 
-  backTop(){
-   this.scrollToTop.nativeElement.scrollTo( 0, 0 );
+  backTop() {
+    this.scrollToTop.nativeElement.scrollTo(0, 0);
   }
 
-  isAdminOrUser(token:any){
-    this.permissions.loadPermissions(token.admin?["ADMIN"]:["USER"]);
-    this.router.navigate(['../home/bacheca'])
-}
+  isAdminOrUser(token: any) {
+    this.permissions.loadPermissions(token.admin ? ['ADMIN'] : ['USER']);
+    this.router.navigate(['../home/bacheca']);
+  }
 
-get username(){
-  return this.authService.loginResponse?.name?? ''
-}
-// OnClickLogOut(){
-//   this.authService.onLogOut()
-//   // this.toastService.inputMethod() // sacro ! 
+  get username() {
+    return this.authService.loginResponse?.name ?? '';
+  }
+  // OnClickLogOut(){
+  //   this.authService.onLogOut()
+  //   // this.toastService.inputMethod() // sacro !
 
-// }
+  // }
 
-
+  getRouteAnimationData() {
+    return this.contexts.getContext('primary')?.route?.snapshot?.data?.[
+      'animation'
+    ];
+  }
 }
