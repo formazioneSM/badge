7b968b5e6152545678312d1e588beadc8f76b7fa
Ilaria <idellaquila@sysmanagement.it>
2022-06-15 16:13:59 +0200

    Modifiche varie

--- src/app/features/home/home-routing.module.ts
+++ src/app/features/home/home-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    
     children: [
       {
         path: 'bacheca',
@@ -44,6 +45,7 @@ const routes: Routes = [
         animation: 'aggiungiPage'
     },
   },
+  { path: 'home', redirectTo: 'home/bacheca' },
 ];
 
 @NgModule({
