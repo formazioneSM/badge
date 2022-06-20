51428dcb53c7c12c3e00f4251863b3645db981c4
Ilaria <idellaquila@sysmanagement.it>
2022-06-20 15:28:24 +0200

    Aggiunta sfumatura overflow card

--- src/app/shared/uikit/components/card/card.component.ts
+++ src/app/shared/uikit/components/card/card.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { types } from 'src/app/shared/utils/constants';
 import { BachecaService } from '../../services/bacheca/bacheca.service';
 import { ToastService } from '../../services/toast/toast.service';
+import { ScrollService } from '../../services/scroll/scroll.service'
 
 @Component({
   selector: 'app-card',
@@ -21,9 +22,13 @@ export class CardComponent implements OnInit, AfterViewInit {
   @Input('isBacheca') isBacheca: any;
   @Output('deletePost') deletePost = new EventEmitter();
   @ViewChild('card') card: ElementRef | any;
-  @ViewChild('heightP') heightP: ElementRef | any;
+  @ViewChild('heightText') heightText: ElementRef | any;
+  @ViewChild('heightTitle') heightTitle: ElementRef | any;
+  @ViewChild('heightLink') heightLink: ElementRef | any;
   calcHeight: any;
   calcHeightText: any;
+  calcHeightTitle: any;
+  calcHeightLink: any;
   isScroll:boolean = false;
 
 
@@ -35,7 +40,7 @@ export class CardComponent implements OnInit, AfterViewInit {
   copy:boolean=true;
   isClicked: boolean = false;
 
-  constructor(private bachecaService: BachecaService, private toastService: ToastService, private router: Router) { 
+  constructor(private bachecaService: BachecaService, private toastService: ToastService, private router: Router, private _scrollService: ScrollService) { 
   }
 
   ngOnInit(): void {
@@ -43,11 +48,14 @@ export class CardComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.calcHeight = this.card.nativeElement.offsetHeight
-    this.calcHeightText = this.heightP.nativeElement.offsetHeight
+    this.calcHeightText = this.heightText.nativeElement.offsetHeight
+    this.calcHeightTitle = this.heightTitle.nativeElement.offsetHeight
+    this.calcHeightLink = this.heightLink.nativeElement.offsetHeight
 
     if(this.calcHeightText > this.calcHeight){
       this.isScroll = true;
     }
+    console.log(this.calcHeight, this.calcHeightText)
   }
   clicked(){
    this.isClicked = true;
@@ -65,4 +73,12 @@ export class CardComponent implements OnInit, AfterViewInit {
     this.router.navigate(['home/aggiungi',this.isBacheca ? this.postId: this.convenzioniId, this.isBacheca ? types.BACHECA : types.CONVENZIONI])
   }
 
+
+  changeScroll(e:any){
+   if(((e.target.clientHeight) + (e.target.scrollTop) ) > this.calcHeightText + this.calcHeightTitle + this.calcHeightLink){
+    this.isScroll = false;
+    } else{
+    this.isScroll = true;
+   }
+  }
 }
